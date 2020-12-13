c47748b325f7922da03039b91c5a2ed356ebfba6
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-12 20:38:35 -0500

    added pseudocode description to the burger.js file for the orm.all section and added <ul> section to the index.handlebars file where the list of burgers should render

--- models/burger.js
+++ models/burger.js
@@ -4,6 +4,7 @@ var orm = require("../config/orm.js");
 //use orm outline to model the burger object
 var burger = {
     all: function(callback) {
+        //use the orm outline and send in relevant table informatin to complete the database request line to display all table information
         orm.all("burgers", function(res) {
             callback(res);
         });
