4bed3a51bd94e0147a74b93c2780a37d72d47d27
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-12 19:35:04 -0500

    added additional connection information to the server.js: use the data in the public folder, use JSON text, use handlebars, use the routes in the controller folder, start the server listening

--- server.js
+++ server.js
@@ -6,3 +6,27 @@ var PORT = process.env.PORT || 8080;
 
 //create instance of express app
 var app = express();
+
+//use the public folder for the static content of the application
+app.use(express.static("public"));
+
+//have application data as JSON text
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//require the handlebars to be used to dynamically display pages
+var exphbs = require("express-handlebars");
+
+app.engint("handlebars", exphbs({defaultLayout: "main"}))
+app.set("view engine", "handlebars");
+
+//pull in the routes from the controller.js file so they can be used by the server
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+//start the server and listen for actions
+app.listen(PORT, function() {
+    //let the use know if the server connection was successful
+    console.log("Server listening on: http://localhost:"+PORT);
+});
