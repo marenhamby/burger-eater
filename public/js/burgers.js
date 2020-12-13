ac54ca9709b082bef3c0f2002188b4b39186ef97
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-13 01:38:10 -0500

    added pseudocode to the burgers.js file for all the on-click functions that the page needs to handle

---
+++ public/js/burgers.js
@@ -0,0 +1,7 @@
+//add handlers to indicate what needs to happen when the different buttons are clicked
+
+//add function behind the button to submit the new burger, add it to the table to be rendered on screen
+
+//add function behind the button to devour the burger so the devoured setting changes to true and it will move to the second table
+
+//add function behind the delete button to remove the burger from the table
