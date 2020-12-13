d1e42da00c74d5b39138620529424dc0b1fde7dc
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-13 01:25:59 -0500

    corrected formatting of comments on schema document

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 -- if the database exists already, then delete it it --
 DROP DATABASE IF EXISTS burger_db;
 
---create the database--
+-- create the database --
 CREATE DATABASE burger_db;
 
---use the burger database for the rest of this document--
+-- use the burger database for the rest of this document --
 USE burger_db;
 
---create the table of burgers--
---include unique id (as the primary key), the name of the burger, and an indication of whether or not the burger has been eaten--
+-- create the table of burgers --
+-- include unique id (as the primary key), the name of the burger, and an indication of whether or not the burger has been eaten --
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(75) NOT NULL,
