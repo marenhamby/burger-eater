6900d9f7d018daf42f9024726b7db77b582dcf23
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-12 19:45:37 -0500

    added express connection and connection to functions in burger.js file into the controller.js file

---
+++ controllers/burgerController.js
@@ -0,0 +1,9 @@
+//require the express module
+var express = require("express");
+
+var router = express.Router();
+
+//use the database functions in the burger.js file
+var burger = require("../models/burger.js");
+
+//add the get, post, put, and delete routes
