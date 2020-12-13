fdb52184d6fea6a0857cbf0bad0a215b63205f48
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-12 22:30:49 -0500

    changed the column reference in the burger.update of teh controller.js file

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res){
     //change the eaten condition of the selected row
     var condition = "id = " + req.params.id;
 
-    burger.update({ eaten: req.body.eaten}, condition, function(result) {
+    burger.update(eaten, condition, function(result) {
         //if nothing was changed, throw error
         if (result.changedRows === 0) {
             return res.status(404).end();
