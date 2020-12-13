ebc137b702a0005bd0ac1c3d69de9c8e97fe4b66
marenh <72942304+marenhamby@users.noreply.github.com>
2020-12-12 20:02:44 -0500

    started outline for get route on the controller.js

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -6,4 +6,8 @@ var router = express.Router();
 //use the database functions in the burger.js file
 var burger = require("../models/burger.js");
 
-//add the get, post, put, and delete routes
+//add the get, post, put, and delete routes to communicate between the client, views, and models
+//ROUTES
+router.get("/", function(req,res) {
+
+});
